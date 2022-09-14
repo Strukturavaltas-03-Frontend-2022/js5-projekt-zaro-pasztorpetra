e7828c502cd6a274efae9f6f853d1b08656b15dd
Petra <pasztor.petra1998@gmail.com>
2022-09-14 23:12:29 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 import { apiUrl } from './settings.js';
+
+// Create a new user
+// // {
+//   "id": 1,
+//   "name": "Lee Garrit",
+//   "emailAddress": "lgarrit0@latimes.com",
+//   "address": "9 Eagan Pass"
+// }
+
+await fetch(`${apiUrl}users`, {
+  method: 'POST',
+  body: JSON.stringify ({"name": "Pásztor Petra","emailAddress": "lgarrit0@latimes.com","address": "9 Eagan Pass"}),
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+
+//Get all users
+const response = await fetch (`${apiUrl}users`);
+const userList = await response.json();
+console.log(userList);
