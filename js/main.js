623a18a26e569263c55e3f692facbb2a037c5c78
Petra <pasztor.petra1998@gmail.com>
2022-09-17 22:31:24 +0200

    table filled with data

--- js/main.js
+++ js/main.js
@@ -21,3 +21,51 @@ await fetch(`${apiUrl}users`, {
 const response = await fetch (`${apiUrl}users`);
 const userList = await response.json();
 console.log(userList);
+
+
+//START
+//GET data from the server
+
+function getServerData (url) {
+  let fetchOptions = {
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache"
+  };
+  return fetch(url, fetchOptions).then(
+    response => response.json(),
+    err => console.error(err)
+  );
+}
+
+getServerData(apiUrl).then(
+  data => fillDataTable(data, "userTable")
+);
+
+//Fill table with server data 
+function fillDataTable(data, tableID) {
+    let table = document.querySelector(`#${tableID}`);
+    if (!table) {
+      console.error(`Table "${tableID}" is not found.`);
+      return;
+    }
+    let tBody = table.querySelector("tbody");
+    for (let row of data) {
+      let tr = createAnyElement("tr");
+      for (let k in row) {
+        let td = createAnyElement("td");
+        td.textContent = row[k];            //innerHTML
+        tr.appendChild(td);
+      }
+      tBody.appendChild(tr);
+    }
+}
+
+function createAnyElement(name, attributes) {
+  let element = document.createElement(name);
+  for (let k in attributes) {
+    element.setAttribute(k, attributes[k]);
+  };
+  return element;
+}
+
